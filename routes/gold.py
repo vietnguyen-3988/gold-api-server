998592f97ef0f8c77c0d47d3fe2803b32e62e6a9
vietnguyen <nguyenquangviet.88@gmail.com>
2025-12-04 22:07:18 +0700

    edit code 3

--- routes/gold.py
+++ routes/gold.py
@@ -6,7 +6,7 @@ gold_bp = Blueprint("gold", __name__)
 @gold_bp.get("/gold")
 def get_gold():
     try:
-        data = requests.get("https://api.exchangerate.host/latest?base=XAU&symbols=USD").json()
+        data = requests.get("http://api.btmc.vn/api/BTMCAPI/getpricebtmc?key=3kd8ub1llcg9t45hnoh8hmn7t5kc2v").json()
         return jsonify({"price": data})
     except Exception as e:
         return jsonify({"error": str(e)}), 500
